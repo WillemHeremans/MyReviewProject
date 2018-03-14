aa0424f336b4452d8787899a2bc125ad2b22cc53
user <mourad.elbouaananii@gmail.com>
2018-03-14 12:37:45 +0100

    change profile.css

--- application/views/pages/profile.php
+++ application/views/pages/profile.php
@@ -2,7 +2,7 @@
 
 </head>
     <body>
-        
+        <p class="mike">Tu dois faire ton HTML ici!</p>
         <h1><?php echo $title; ?></h1>
 <form action="http://localhost/MyReview/MyReviewProject/">
     <input type="submit" value="Go Home" />
