b70de7b4ee7dffcfd7b4aa4057ac01c7dd3519e4
user <mourad.elbouaananii@gmail.com>
2018-03-20 11:26:35 +0100

    Indent News.php

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -32,29 +32,30 @@ class News extends CI_Controller {
                 $this->load->view('templates/header', $data);
                 $this->load->view('news/view', $data);
                 $this->load->view('templates/footer');
-                }
-                public function create()
+        }
+
+        public function create()
+        {
+                $this->load->helper('form');
+                $this->load->library('form_validation');
+                
+                $data['title'] = 'Create a news item';
+                
+                $this->form_validation->set_rules('title', 'Title', 'required');
+                $this->form_validation->set_rules('text', 'Text', 'required');
+                
+                if ($this->form_validation->run() === FALSE)
                 {
-                    $this->load->helper('form');
-                    $this->load->library('form_validation');
-                
-                    $data['title'] = 'Create a news item';
-                
-                    $this->form_validation->set_rules('title', 'Title', 'required');
-                    $this->form_validation->set_rules('text', 'Text', 'required');
-                
-                    if ($this->form_validation->run() === FALSE)
-                    {
                         $this->load->view('templates/header', $data);
                         $this->load->view('news/create');
                         $this->load->view('templates/footer');
                 
-                    }
-                    else
-                    {
+                }
+                else
+                {
                         $this->news_model->set_news();
                         $this->load->view('news/success');
-                    }
                 }
+        }
 
 }
