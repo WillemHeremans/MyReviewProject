939d96312c82b41e1e5efbd581baa27c44a46a3d
Va2 <vadim.brieven@gmail.com>
2018-03-22 12:05:09 +0100

    Fix CDN links

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,9 +1,4 @@
     <title>Login</title>
-    <!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="./css/bootstrap-social.css">
-
     <link rel="stylesheet" href="./css/<?php echo $title; ?>.css">
 </head>
 <body>
@@ -92,11 +87,6 @@
             </header>
         </div>
     </div>
-
-    <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- Bootstrap JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 
     <form action="<?php echo base_url(); ?>/profile">
