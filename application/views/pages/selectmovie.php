39f771ace4b955108f28bc64560c17dbd95c8ea2
Va2 <vadim.brieven@gmail.com>
2018-03-22 12:01:07 +0100

    Bootstrap containers

--- application/views/pages/selectmovie.php
+++ application/views/pages/selectmovie.php
@@ -1,9 +1,4 @@
-<title>Select Movie</title>
-    <!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="./css/bootstrap-social.css">
-
+    <title>Select Movie</title>
     <link rel="stylesheet" href="./css/<?php echo $title; ?>.css">
 </head>
 <body>
@@ -22,20 +17,47 @@
         <h1>select your photo</h1>
         <button class="send " data-counter="0">✔</button>
     </div>
-    <ul>
-        <li><img src="https://farm8.staticflickr.com/7326/11287113923_57d37ed9d3_q.jpg" /></li>
-        <li><img src="https://farm9.staticflickr.com/8184/8095683964_9e27753908_q.jpg" /></li>
-        <li><img src="https://farm9.staticflickr.com/8171/8018956825_67bf62c098_q.jpg" /></li>
-        <li><img src="https://farm9.staticflickr.com/8425/7587724752_cdb9f0c444_q.jpg" /></li>
-        <li><img src="https://farm8.staticflickr.com/7248/7587738254_707a32f27b_q.jpg" /></li>
-        <li><img src="https://farm9.staticflickr.com/8191/8095680852_893f685cbd_q.jpg" /></li>
-        <li><img src="https://farm9.staticflickr.com/8460/8018953043_c6ef9e3b29_q.jpg" /></li>
-        <li><img src="https://farm9.staticflickr.com/8026/7445019824_914dea4ac3_q.jpg" /></li>
-        <li><img src="https://farm8.staticflickr.com/7088/7332137562_14956827a7_q.jpg" /></li>
-        <li><img src="https://farm8.staticflickr.com/7108/7151306497_9cfb1a367b_q.jpg" /></li>
-        <li><img src="https://farm6.staticflickr.com/5198/7005209880_432389ef25_q.jpg" /></li>
-        <li><img src="https://farm8.staticflickr.com/7280/7151302883_e6ef32f04d_q.jpg" /></li>
-    </ul>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-4">
+                <ul>
+                    <li><img src="https://farm8.staticflickr.com/7326/11287113923_57d37ed9d3_q.jpg" /></li>
+                    <li><img src="https://farm9.staticflickr.com/8184/8095683964_9e27753908_q.jpg" /></li>
+                    <li><img src="https://farm9.staticflickr.com/8171/8018956825_67bf62c098_q.jpg" /></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+        <div class="container">
+        <div class="row">
+            <div class="col-xs-4">
+                <ul>
+                    <li><img src="https://farm8.staticflickr.com/7326/11287113923_57d37ed9d3_q.jpg" /></li>
+                    <li><img src="https://farm9.staticflickr.com/8184/8095683964_9e27753908_q.jpg" /></li>
+                    <li><img src="https://farm9.staticflickr.com/8171/8018956825_67bf62c098_q.jpg" /></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+<li><img src="https://farm9.staticflickr.com/8425/7587724752_cdb9f0c444_q.jpg" /></li>
+                    <li><img src="https://farm8.staticflickr.com/7248/7587738254_707a32f27b_q.jpg" /></li>
+                    <li><img src="https://farm9.staticflickr.com/8191/8095680852_893f685cbd_q.jpg" /></li>
+                    <li><img src="https://farm9.staticflickr.com/8460/8018953043_c6ef9e3b29_q.jpg" /></li>
+                    <li><img src="https://farm9.staticflickr.com/8026/7445019824_914dea4ac3_q.jpg" /></li>
+                    <li><img src="https://farm8.staticflickr.com/7088/7332137562_14956827a7_q.jpg" /></li>
+                    <li><img src="https://farm8.staticflickr.com/7108/7151306497_9cfb1a367b_q.jpg" /></li>
+                    <li><img src="https://farm6.staticflickr.com/5198/7005209880_432389ef25_q.jpg" /></li>
+                    <li><img src="https://farm8.staticflickr.com/7280/7151302883_e6ef32f04d_q.jpg" /></li>
+
+
 
 
     <!-- jQuery -->
