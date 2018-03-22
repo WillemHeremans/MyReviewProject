13184d5942d816701e8e96f79d6013f51c37a740
user <mourad.elbouaananii@gmail.com>
2018-03-22 12:06:24 +0100

    Display all profils data

--- application/views/tests/profiles.php
+++ application/views/tests/profiles.php
@@ -1,11 +1,23 @@
-<!-- <h2><?php echo $title; ?></h2> -->
-<h1>Heyyyyy</h1>
+<h2><?php echo $title; ?></h2>
 
 <?php foreach ($profiles as $profiles_item): ?>
 
-        <h3><?php echo $profiles_item['pseudo']; ?></h3>
+        <h3>
+                <?php echo $profiles_item['pseudo']; ?>
+                <?php if ($profiles_item['gender']=="male") {
+                        echo $profiles_item['gender'];
+                } else {
+                        echo "lol";
+                }
+                  ?>
+        </h3>
         <div class="main">
                 <?php echo $profiles_item['age']; ?>
+                <?php echo $profiles_item['pref_1']; ?>
+                <?php echo $profiles_item['pref_2']; ?>
+                <?php echo $profiles_item['pref_3']; ?>
+                <?php echo $profiles_item['pref_4']; ?>
+                <?php echo $profiles_item['pref_5']; ?>
         </div>
         <!-- <p><a href="<?php echo site_url('/news/'.$news_item['slug']); ?>">View article</a></p> -->
 
